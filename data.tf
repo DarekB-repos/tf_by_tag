11661786a438f8cca9b7c2c94f5cc9b6bdcaa76a
Dariusz Bernys <dariusz.bernys@ecs.co.uk>
2022-10-20 14:56:45 +0200

    add lambda stuff

--- data.tf
+++ data.tf
@@ -6,3 +6,20 @@ data "aws_resourcegroupstaggingapi_resources" "existing" {
   }
 }
 
+
+
+data "aws_resourcegroupstaggingapi_resources" "kvs_lambda_arn" {
+  resource_type_filters =["lambda:function"]
+  tag_filter {
+    key = "aws:cloudformation:logical-id"
+    values=["KvsProcessRecordingLambdaFunction"]
+  }
+}
+
+data "aws_lambda_function" "dKvSFuntion" {
+  function_name =  data.aws_resourcegroupstaggingapi_resources.kvs_lambda_arn.resource_tag_mapping_list.*.resource_arn[0]
+}
+
+data "aws_iam_role" "dKvsIamRole" {
+  name = split("/",data.aws_lambda_function.dKvSFuntion.role)[1]
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
-output "arns" {
+output "bucket_arn" {
   value = data.aws_resourcegroupstaggingapi_resources.existing.resource_tag_mapping_list.*.resource_arn
 }
+
+
+output "lambda_arn" {
+  value = data.aws_resourcegroupstaggingapi_resources.kvs_lambda_arn.resource_tag_mapping_list.*.resource_arn[0]
+}
+output "lamdbaIamRoleArn" {
+  value = data.aws_iam_role.dKvsIamRole
+}
+
